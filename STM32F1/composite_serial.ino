387f24aceda1357e211832b027b90f5f7f9b7a45
Rick Kimball <rick@kimballsoftware.com>
2019-01-03 14:59:56 -0500

    Comment changes.

--- STM32F1/composite_serial.ino
+++ STM32F1/composite_serial.ino
@@ -1,13 +1,19 @@
 /*
    composite_serial - bluepill composite serial device with reset support
 
-   The 
-   tested using "Serial" upload method which doesn't configure builtin usb serial
-   This code allows you to leave the BOOT0 pin tied high and toggle the reset button to
-   upload via the USART0 (PA9/PA10) on /dev/ttyUSB0, then connect to /dev/ttyACM0 
-   with putty without having to mess with the BOOT0 pin during development.
-   The USBCompositeSerial object normally doesn't toggle the PA12 pin.  
-   This behaviour is bluepill specific. (proper bluepills only have a 1k5 pull up on PA12)
+   The "Serial" upload method disables native USB support.  This example uses the
+   USBComposite library to create a CDC acm virtual COM port instead of using the
+   Maple USB serial.  This is much more flexible than the Maple serial device.
+   
+   This code makes it easier to work with the USART ROM serial bootloader by forcing
+   a USB reset when the bluepill reset button is pressed.  It allows you to leave the
+   BOOT0 pin tied high and toggle the reset button to upload via the USART0 (PA9/PA10)
+   on /dev/ttyUSB0. Use putty to connect to the virtual COM port on /dev/ttyACM0 
+   without having to mess with the BOOT0 pin during development.
+   
+   The USBCompositeSerial object normally doesn't toggle the PA12 pin. This usb reset code
+   here is bluepill specific. Properly configured bluepills only have a 1k5 pull up on PA12.
+   They don't have a transitor to provide USB disconnect. 
    
    Compatible with the libmaple/stm32duino core @ https://github.com/rogerclarkmelbourne/Arduino_STM32/
 */
